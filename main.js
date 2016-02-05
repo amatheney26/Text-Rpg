78d5fbf404b32a523c898aa2698c73304cb537c6
Andrew Matheney <amatheney26@gmail.com>
2016-02-05 18:52:36 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+explain = function() {
+  window.location.replace('htmls/explain.html');
+}
+
+exit = function() {
+  window.location.replace('https://www.google.com');
+}
+Continue1 = function() {
+  window.location.replace('Option1.html');
+}
+Option1yes = function() {
+  window.location.replace('Option4.html');
+}
+Option1no = function() {
+  window.location.replace('Option3.html');
+}
